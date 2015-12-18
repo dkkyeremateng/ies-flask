1cff03ebaca4b05851a6797722067a5f79879845
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2015-12-18 15:59:53 +0000

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "HELLO12345WORLD"
+DEBUG = True
+DB_USERNAME = 'root'
+DB_PASSWORD = ""
+BLOG_DATABASE_NAME = "flask"
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/flask"
+MAIL_USERNAME = "kofwayne305@gmail.com"
+MAIL_PASSWORD = 'GODwagious305'
